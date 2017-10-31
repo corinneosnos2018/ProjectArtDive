056d14e488c8ee61d28c0850288a67d32e9638e7
Andrew Lapin <alapin@nuit-50.44mont.gvad.net>
2017-10-31 13:10:46 -0700

    minor tweak to page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 // get scripting
-$(document).ready(function(){})
-
-
-
-$(function(){
-  alert($);  
-})
 $(document).ready(function(){
 	var i=0;
-$("typezone").(function())
-$("button").click(function(){
-    	i++; // increment "i" by +1
-        $("typezone",{id:i}).appendTo("#chat");
-    var textfield = $("input:text").val();
-    $("#"+i).text(textfield);
-    $("input:text").val("").focus();
+
+    $("button").click(function(){
+    	submitComment();
+    });
     
+    $("input:text").keypress(function (e) {
+        if (e.which == 13) {
+           submitComment();
+        }
+    });
+    
+    function submitComment(){
+        i++; // increment "i" by +1
+        $("<div>",{class:"container",id:i}).appendTo("#chat");
+        var txt = $("input:text").val();
+        $("#"+i).text(txt);
+        $("input:text").val("").focus();
+    }
+    
+});
